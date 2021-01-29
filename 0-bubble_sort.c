7f0d1e6e0830f8b75327526978fb4a825ee9c0d9
laurendobratz00 <laurendobratz@icloud.com>
2021-01-29 18:55:28 +0000

    Add files

---
+++ 0-bubble_sort.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * bubble_sort - bubble sorting algorithm
+ * @array: int
+ * @size: size_t
+ * Return: void
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i, j, temp;
+
+	for (i = 0; i < (size - 1); i++)
+		for (j = 0; j < (size - i - 1); j++)
+			if (array[j] > array[j + 1])
+			{
+				temp = *&array[j];
+				*&array[j] = *&array[j + 1];
+				*&array[j + 1] = temp;
+				print_array(array, size);
+			}
+}
