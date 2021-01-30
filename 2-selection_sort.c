cc427535ed4874a47ee82adff6623063612cee7b
laurendobratz00 <laurendobratz@icloud.com>
2021-01-30 17:38:28 +0000

    add files

---
+++ 2-selection_sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+
+/**
+ * selection_sort - selection sorting algorithm
+ * @array: int pointer
+ * @size: struct size_t
+ * Return: void
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i, j, min, temp;
+
+	for (i = 0; i < (size - 1); i++)
+	{
+		min = i;
+		for (j = (i + 1); j < size; j++)
+			if (array[j] < array[min])
+				min = j;
+		temp = *&array[min];
+		*&array[min] = *&array[i];
+		*&array[i] = temp;
+		print_array(array, size);
+	}
+
+}
