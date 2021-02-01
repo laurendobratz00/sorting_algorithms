19192a7af9c0f1b9c0a0c58fe4ed58316b91ad35
laurendobratz00 <laurendobratz@icloud.com>
2021-02-01 15:58:38 +0000

    add if statement

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,6 +13,8 @@ void bubble_sort(int *array, size_t size)
 {
 	unsigned int i, j, temp;
 
+	if (size < 2)
+		return;
 	for (i = 0; i < (size - 1); i++)
 		for (j = 0; j < (size - i - 1); j++)
 			if (array[j] > array[j + 1])
