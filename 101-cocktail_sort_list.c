33d479911b67a1aa2e94ef63702fd0ce29740bdd
laurendobratz00 <laurendobratz@icloud.com>
2021-02-02 15:27:45 +0000

    add file

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * cocktail_sort_list - cocktail sorting algorithm
+ * @list: double pointer to a list of ints
+ * Return: void
+ */
+
+void cocktail_sort_list(listint_t **list)
+{
+	listint_t *current = NULL;
+
+	if (list == NULL || *list == NULL)
+		return;
+
+	current = (*list)->next;
+
+	for (; current != NULL; current = current->next)
+	{
+		while (current->prev != NULL && current->n < current->prev->n)
+		{
+			current->prev->next = current->next;
+			if (current->next != NULL)
+				current->next->prev = current->prev;
+
+			current->next = current->prev;
+			current->prev = current->prev->prev;
+			current->next->prev = current;
+
+			if (current->prev == NULL)
+				*list = current;
+			else
+				current->prev->next = current;
+			print_list(*list);
+		}
+	}
+}
