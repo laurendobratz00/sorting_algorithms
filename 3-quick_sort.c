20b461f46d5fd1b36e5b01f1d3aa3edc77972a60
laurendobratz00 <laurendobratz@icloud.com>
2021-02-03 16:17:15 +0000

    changed null

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -61,9 +61,9 @@ void quick_sort(int *array, size_t size)
 {
 	int low = 0, high = size - 1;
 
-	if (size < 2)
-		return;
 	if (array == NULL)
+		return;
+	if (size < 2)
 		return;
 	partition(array, low, high, size);
 }
